e67c1e466c57ed4d255103a52ca25a5d2795f32b
Andrew Cistola MPH <andrewcistola@pm.me>
2021-08-16 22:57:20 -0400

    Add files via upload

---
+++ _fig/style.r
@@ -0,0 +1,8 @@
+low = 'blue3' # Standard map colorscheme
+mid = 'lightyellow' # Standard map colorscheme
+high = 'red3' # Standard map colorscheme
+na = 'grey50' # Standard map colorscheme
+line = 'black' # Standard map colorschem
+breaks = 9 # Standard map colorscheme
+scheme = 'trad' # Standard map colorscheme
+font = 'Vollkorn' # Define font for all figs 
